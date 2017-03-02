e5a12dbf696deb70e6e4cbfd64d1490390c54b00
Simon Hurley <simon.hurley@elateral.com>
2017-03-02 14:55:28 +0000

    fixed build

--- teamcity.bat
+++ teamcity.bat
@@ -1,8 +1,9 @@
+call npm install
 call npm install -g swagger-diff
 call path=%%PATH%%;%%APPDATA%%\npm
 call npm run test --silent && exit 0
 call npm run run
-rem call npm install
+
 
 
 rem call swagger-diff https://%1/%3/api-docs https://%2/%3/api-docs
