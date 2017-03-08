7a512e89213831117b208b66851582ecd6dcdf0a
Simon Hurley <simon.hurley@mail.com>
2017-03-08 16:06:57 +0000

    added debug info to teamcity.bat

--- teamcity.bat
+++ teamcity.bat
@@ -1,14 +1,18 @@
 @ECHO OFF
+setlocal ENABLEDELAYEDEXPANSION
 if "%DONOTRUNONENVIRONMENT%" == "true" (
 	echo ##teamcity[buildStatus status='NotSuitable' text='These tests are not suitable for this environment']
 ) else (
 	set oldApiUrl=https://%1/%3/api-docs
 	set newApiUrl=https://%2/%3/api-docs
 
+	echo oldApiUrl=https://%1/%3/api-docs
+	echo newApiUrl=https://%2/%3/api-docs
 	call npm install
 	call npm install -g swagger-diff
 	call path=%%PATH%%;%%APPDATA%%\npm
-	call swagger-diff -o testreport.json %oldApiUrl% %newApiUrl%
+	echo swagger-diff -o testreport.json !oldApiUrl! !newApiUrl!
+	call swagger-diff -o testreport.json !oldApiUrl! !newApiUrl!
 	call npm run run --oldApiHost=%1 --newApiHost=%2 --apiVersion=%3
 
 	set "oldApiUrl="
