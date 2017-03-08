6de9edf71a847f8ec0f0c4b8d7214c326bb843dd
Simon Hurley <simon.hurley@mail.com>
2017-03-08 15:09:13 +0000

    added env var to prevent tests running if environment is unsuitable

--- teamcity.bat
+++ teamcity.bat
@@ -1,11 +1,16 @@
-set oldApiUrl=https://%1/%3/api-docs
-set newApiUrl=https://%2/%3/api-docs
+@ECHO OFF
+if "%DONOTRUNONENVIRONMENT%" == "true" (
+	echo ##teamcity[buildStatus status='NotSuitable' text='These tests are not suitable for this environment']
+) else (
+	set oldApiUrl=https://%1/%3/api-docs
+	set newApiUrl=https://%2/%3/api-docs
 
-call npm install
-call npm install -g swagger-diff
-call path=%%PATH%%;%%APPDATA%%\npm
-call swagger-diff -o testreport.json %oldApiUrl% %newApiUrl%
-call npm run run --oldApiHost=%1 --newApiHost=%2 --apiVersion=%3
+	call npm install
+	call npm install -g swagger-diff
+	call path=%%PATH%%;%%APPDATA%%\npm
+	call swagger-diff -o testreport.json %oldApiUrl% %newApiUrl%
+	call npm run run --oldApiHost=%1 --newApiHost=%2 --apiVersion=%3
 
-set "oldApiUrl="
-set "newApiUrl="
+	set "oldApiUrl="
+	set "newApiUrl="
+)
